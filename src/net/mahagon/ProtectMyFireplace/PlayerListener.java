8aedd2df89c2a8be1df273b886dcc3b09cd24e43
Mahagon <mail@mahagon.net>
2015-01-26 14:31:16 +0100

    Disable simple fireprotection in nether only Fireplaces with trapdoors are protected

--- src/net/mahagon/ProtectMyFireplace/PlayerListener.java
+++ src/net/mahagon/ProtectMyFireplace/PlayerListener.java
@@ -2,6 +2,7 @@ package net.mahagon.ProtectMyFireplace;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -17,12 +18,15 @@ public class PlayerListener implements Listener {
 			Player player = event.getPlayer();
 			Block block = player.getTargetBlock(null, 5);
 			if(block != null && block.getType().equals(Material.FIRE) && block.getRelative(BlockFace.DOWN).getType().equals(Material.NETHERRACK)){
-				player.sendMessage(ChatColor.RED + "[PMF] Fire above netherrack is protected, you need to break the netherblock below");
 				if(!(block.getRelative(BlockFace.EAST).getType().equals(Material.TRAP_DOOR) || block.getRelative(BlockFace.WEST).getType().equals(Material.TRAP_DOOR) || block.getRelative(BlockFace.SOUTH).getType().equals(Material.TRAP_DOOR) || block.getRelative(BlockFace.NORTH).getType().equals(Material.TRAP_DOOR))){
+					if(player.getWorld().getEnvironment().equals(World.Environment.NETHER)){
+						return;
+					}
 					block.setType(Material.FIRE);	
 				} else {
 					block.setTypeId(Material.FIRE.getId(),false);
 				}
+				player.sendMessage(ChatColor.RED + "[PMF] Fire above netherrack is protected, you need to break the netherblock below");
 				event.setCancelled(true);
 				return;
 			}
