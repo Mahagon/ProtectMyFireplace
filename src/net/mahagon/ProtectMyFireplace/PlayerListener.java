703d18a4454271c24fe84b7f0887d98e09284fa5
Mahagon <mail@mahagon.net>
2015-04-03 13:59:12 +0200

    getTargetBlock was not reliable

--- src/net/mahagon/ProtectMyFireplace/PlayerListener.java
+++ src/net/mahagon/ProtectMyFireplace/PlayerListener.java
@@ -16,8 +16,8 @@ public class PlayerListener implements Listener {
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if(event.getAction().equals(Action.LEFT_CLICK_BLOCK)){
 			Player player = event.getPlayer();
-			Block block = player.getTargetBlock(null, 5);
-			if(block != null && block.getType().equals(Material.FIRE) && block.getRelative(BlockFace.DOWN).getType().equals(Material.NETHERRACK)){
+			Block block = event.getClickedBlock().getRelative(event.getBlockFace());
+			if(block.getType().equals(Material.FIRE) && block.getRelative(BlockFace.DOWN).getType().equals(Material.NETHERRACK)){
 				if(!(block.getRelative(BlockFace.EAST).getType().equals(Material.TRAP_DOOR) || block.getRelative(BlockFace.WEST).getType().equals(Material.TRAP_DOOR) || block.getRelative(BlockFace.SOUTH).getType().equals(Material.TRAP_DOOR) || block.getRelative(BlockFace.NORTH).getType().equals(Material.TRAP_DOOR))){
 					if(player.getWorld().getEnvironment().equals(World.Environment.NETHER)){
 						return;
