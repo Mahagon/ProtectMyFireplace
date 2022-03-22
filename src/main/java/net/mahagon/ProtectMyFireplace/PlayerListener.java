5b0288cdfe056e35d7e608691c8706b982d3a0ea
Mahagon <mail@danieljacobs.de>
2022-03-22 23:48:02 +0100

    🚨 replaced tabs with spaces

--- src/main/java/net/mahagon/ProtectMyFireplace/PlayerListener.java
+++ src/main/java/net/mahagon/ProtectMyFireplace/PlayerListener.java
@@ -11,31 +11,31 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerListener implements Listener {
-	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent event) {
-		if (!event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
-			return;
-		}
-		Block block = event.getClickedBlock();
-		if (!block.getType().equals(Material.FIRE)) {
-			return;
-		}
-		if (
-			!(
-				block.getRelative(BlockFace.EAST).getType().name().endsWith("TRAPDOOR") ||
-				block.getRelative(BlockFace.WEST).getType().name().endsWith("TRAPDOOR") ||
-				block.getRelative(BlockFace.NORTH).getType().name().endsWith("TRAPDOOR") ||
-				block.getRelative(BlockFace.SOUTH).getType().name().endsWith("TRAPDOOR")
-			)
-		) {
-			return;
-		}
-		if (block.getRelative(BlockFace.DOWN).getType().equals(Material.NETHERRACK)) {
-			Player player = event.getPlayer();
-			player.sendMessage(ChatColor.RED + "[PMF] Fire above netherrack is protected, you need to break the netherblock below");
-			event.setCancelled(true);
-			return;
-		}
-		block.setType(Material.FIRE, false);
-	}
+  @EventHandler
+  public void onPlayerInteract(PlayerInteractEvent event) {
+    if (!event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+      return;
+    }
+    Block block = event.getClickedBlock();
+    if (!block.getType().equals(Material.FIRE)) {
+      return;
+    }
+    if (
+      !(
+        block.getRelative(BlockFace.EAST).getType().name().endsWith("TRAPDOOR") ||
+        block.getRelative(BlockFace.WEST).getType().name().endsWith("TRAPDOOR") ||
+        block.getRelative(BlockFace.NORTH).getType().name().endsWith("TRAPDOOR") ||
+        block.getRelative(BlockFace.SOUTH).getType().name().endsWith("TRAPDOOR")
+      )
+    ) {
+      return;
+    }
+    if (block.getRelative(BlockFace.DOWN).getType().equals(Material.NETHERRACK)) {
+      Player player = event.getPlayer();
+      player.sendMessage(ChatColor.RED + "[PMF] Fire above netherrack is protected, you need to break the netherblock below");
+      event.setCancelled(true);
+      return;
+    }
+    block.setType(Material.FIRE, false);
+  }
 }
--- src/main/java/net/mahagon/ProtectMyFireplace/ProtectMyFireplacePlugin.java
+++ src/main/java/net/mahagon/ProtectMyFireplace/ProtectMyFireplacePlugin.java
@@ -6,28 +6,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ProtectMyFireplacePlugin extends JavaPlugin
 {
-	private static Plugin plugin;
+  private static Plugin plugin;
     /**
      * Called on plugin enable.
      */
-	@Override
-	public void onEnable() {
-		PlayerListener playerListener = new PlayerListener();
-		Bukkit.getServer().getPluginManager().registerEvents(playerListener, this);
-		BlockListener blockListener = new BlockListener();
-		Bukkit.getServer().getPluginManager().registerEvents(blockListener, this);
-	}
+  @Override
+  public void onEnable() {
+    PlayerListener playerListener = new PlayerListener();
+    Bukkit.getServer().getPluginManager().registerEvents(playerListener, this);
+    BlockListener blockListener = new BlockListener();
+    Bukkit.getServer().getPluginManager().registerEvents(blockListener, this);
+  }
     /**
      * Called on plugin disable.
      */
     @Override
-	public void onDisable(){
-    	plugin = null;
-	}
-		/**
+  public void onDisable(){
+      plugin = null;
+  }
+    /**
      * Called on Command.
      */
     public static Plugin getPlugin() {
-    	return plugin;
+      return plugin;
     }
 }
