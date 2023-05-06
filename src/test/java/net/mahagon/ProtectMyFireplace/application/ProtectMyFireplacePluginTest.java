c73ffebc0d6c8b6cef4076ced98613615f9cffd0
Daniel Jacobs <mail@danieljacobs.de>
2023-05-06 10:36:00 +0200

    ✅ write tests for the plugin
    Signed-off-by: Daniel Jacobs <mail@danieljacobs.de>

---
+++ src/test/java/net/mahagon/ProtectMyFireplace/application/ProtectMyFireplacePluginTest.java
@@ -0,0 +1,30 @@
+package net.mahagon.ProtectMyFireplace;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+class ProtectMyFireplacePluginTest {
+
+  @Test
+  void onEnable() {
+    PluginManager pluginManager = Mockito.mock(PluginManager.class);
+    JavaPlugin plugin = Mockito.mock(JavaPlugin.class);
+
+    ProtectMyFireplacePlugin protectMyFireplacePlugin = new ProtectMyFireplacePlugin();
+    protectMyFireplacePlugin.onEnable();
+
+    assertNotNull(ProtectMyFireplacePlugin.getInstance());
+  }
+
+  @Test
+  void onDisable() {
+    ProtectMyFireplacePlugin protectMyFireplacePlugin = new ProtectMyFireplacePlugin();
+    protectMyFireplacePlugin.onDisable();
+
+    assertNull(ProtectMyFireplacePlugin.getInstance());
+  }
+}
